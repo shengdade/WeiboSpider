93fa4f80a42a84a27b072a157cb7dc42f2f490b4
Dade Sheng <dade@live.cn>
2017-01-22 17:57:06 -0500

    wrap process to main function

--- weiboSpider.py
+++ weiboSpider.py
@@ -1,4 +1,8 @@
 # -*-coding:gbk-*-
+
+# http://www.cognize.me/2016/03/26/weibospider/?utm_source=tuicool&utm_medium=referral
+# https://github.com/dataabc/weiboSpider
+
 import codecs
 import os
 import re
@@ -162,17 +166,18 @@ class Weibo:
             traceback.print_exc()
 
 
-# 使用实例,输入一个用户id，所有信息都会存储在wb实例中
-user_id = 1729370543  # 可以改成任意合法的用户id（爬虫的微博id除外）
-user_filter = 1  # 值为0表示爬取全部的微博信息（原创微博+转发微博），值为1表示只爬取原创微博
-wb = Weibo(user_id, user_filter)  # 调用weibo类，创建微博实例wb
-wb.start()  # 爬取微博信息
-print('用户名：' + wb.userName)
-print('全部微博数：' + str(wb.weiboNum))
-print('关注数：' + str(wb.following))
-print('粉丝数：' + str(wb.followers))
-print('最新一条微博为：' + wb.weibos[0])  # 若filter=1则为最新的原创微博，如果该用户微博数为0，即len(wb.weibos)==0,打印会出错，下同
-print('最新一条微博获得的点赞数：' + str(wb.num_zan[0]))
-print('最新一条微博获得的转发数：' + str(wb.num_forwarding[0]))
-print('最新一条微博获得的评论数：' + str(wb.num_comment[0]))
-wb.write_txt()  # wb.writeTxt()只是把信息写到文件里，大家可以根据自己的需要重新编写writeTxt()函数
+if __name__ == '__main__':
+    # 使用实例,输入一个用户id，所有信息都会存储在wb实例中
+    user_id = 1669879400  # 可以改成任意合法的用户id（爬虫的微博id除外）
+    user_filter = 1  # 值为0表示爬取全部的微博信息（原创微博+转发微博），值为1表示只爬取原创微博
+    wb = Weibo(user_id, user_filter)  # 调用weibo类，创建微博实例wb
+    wb.start()  # 爬取微博信息
+    print('用户名：' + wb.userName)
+    print('全部微博数：' + str(wb.weiboNum))
+    print('关注数：' + str(wb.following))
+    print('粉丝数：' + str(wb.followers))
+    print('最新一条微博为：' + wb.weibos[0])  # 若filter=1则为最新的原创微博，如果该用户微博数为0，即len(wb.weibos)==0,打印会出错，下同
+    print('最新一条微博获得的点赞数：' + str(wb.num_zan[0]))
+    print('最新一条微博获得的转发数：' + str(wb.num_forwarding[0]))
+    print('最新一条微博获得的评论数：' + str(wb.num_comment[0]))
+    wb.write_txt()  # wb.writeTxt()只是把信息写到文件里，大家可以根据自己的需要重新编写writeTxt()函数
